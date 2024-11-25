0ed11b0b8911d3bf1d9dc924443374a1583a426a
MRidhoPS <mridhophs@gmail.com>
2024-11-25 21:11:38 +0700

    add new projects

--- lib/database/dummy_data.dart
+++ lib/database/dummy_data.dart
@@ -1,20 +1,27 @@
 const data = [
   {
-    'id': 1,
+    'title': 'SIMS PPOB',
+    'tech': 'Flutter, Swagger',
+    'image': 'assets/ppob.png',
+  },
+  {
+    'title': 'Posrem Web',
+    'tech': 'Flutter, Firebase',
+    'image': 'assets/posrem.png',
+  },
+  {
     'title': 'Students Attendance',
     'tech': 'Flutter, Express.js, MySQL',
     'image': 'assets/sa1.png',
   },
   {
-    'id': 2,
     'title': "Al-Quran",
     'tech': 'Flutter',
     'image': 'assets/alquran1.png'
   },
   {
-    'id': 3,
     'title': "Money Trackers",
     'tech': 'Flutter, Express.js, MySQL',
-    'image': 'assets/mt1.png',
+    'image': 'assets/mt2.png',
   }
 ];
